3062f83ce8266af25bb3ba0e28b1405bdf680162
Tyler <brown.tyler.w@gmail.com>
2024-01-14 22:05:38 -0800

    Ship module now works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import loadUI from "./ui";
 import createGrid from "./modules/grid";
+import Ship from "./modules/ship";
 import './styles.css'
 
 //document.body.appendChild(loadUI());
 document.body.appendChild(createGrid());
+// Ensuring module works
+const ship = require('./modules/ship');
+const battleship = new ship('battleship', 4);
+battleship.hit();
+console.log(battleship.isSunk());
+battleship.hit();
+console.log(battleship.isSunk());
+battleship.hit();
+console.log(battleship.isSunk());
+battleship.hit();
+// Ship is hit 4 times, therefore ship is sunk
+console.log(battleship.isSunk());
+console.log(battleship.type);
---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+// Ship module. Contains all necessary information/functions to create a new ship
+function Ship(type, length) {
+    this.type = type;
+    this.length = length;
+    this.hit_count = 0;
+    this.sunk = false;
+    this.hit = function() {
+        this.hit_count++;
+    }
+    this.isSunk = function() {
+        if (this.hit_count === this.length) {
+            this.sunk = true;
+        }
+        return this.sunk;
+    }
+}
+
+module.exports = Ship;
